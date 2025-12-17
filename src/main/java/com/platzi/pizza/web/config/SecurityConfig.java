90a5a06d9d7c0b6643b10efb13fad78013ed9354
Felipe Morales <fm.moralespro@gmail.com>
2025-12-17 17:43:58 -0300

    Configuración de CORS en Spring Security para APIs y Frontend

--- src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
+++ src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
@@ -11,6 +11,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http    .csrf().disable()
+                .cors().and()
                 .authorizeHttpRequests()
                 .anyRequest()
                 //.permitAll();
