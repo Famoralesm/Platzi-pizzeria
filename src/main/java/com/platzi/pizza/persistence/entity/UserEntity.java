40e00d47a112303ea30d33f2d3c9702c68e865f1
Felipe Morales <fm.moralespro@gmail.com>
2025-12-18 14:24:05 -0300

    Asignación de Roles y Permisos en Spring Security

---
+++ src/main/java/com/platzi/pizza/persistence/entity/UserEntity.java
@@ -0,0 +1,35 @@
+package com.platzi.pizza.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name="user", schema = "pizzeria")
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserEntity {
+
+    @Id
+    @Column(nullable = false,length = 20)
+    private String username;
+
+    @Column(nullable = false,length = 200)
+    private String password;
+
+    @Column(length = 50)
+    private String email;
+
+    @Column(nullable = false)
+    private Boolean locked;
+
+    @Column(nullable = false)
+    private Boolean disabled;
+
+    @OneToMany(mappedBy = "user", fetch=FetchType.EAGER)
+    private List<UserRoleEntity> roles;
+}
---
+++ src/main/java/com/platzi/pizza/service/UserSecurityService.java
@@ -0,0 +1,39 @@
+package com.platzi.pizza.service;
+
+import com.platzi.pizza.persistence.entity.UserEntity;
+import com.platzi.pizza.persistence.entity.UserRoleEntity;
+import com.platzi.pizza.persistence.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserSecurityService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserSecurityService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findById(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        String[] roles = userEntity.getRoles().stream().map(UserRoleEntity::getRole).toArray(String[]::new);
+
+        // Construir un UserDetails usando los valores de userEntity
+        return User.builder()
+                .username(userEntity.getUsername())
+                .password(userEntity.getPassword())
+                .roles(roles) // Temporalmente se asigna el rol de 'admin'
+                .accountLocked(userEntity.getLocked())
+                .disabled(userEntity.getDisabled())
+                .build();
+    }
+}
